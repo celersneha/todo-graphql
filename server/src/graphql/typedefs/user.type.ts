76eb6441ea04d5a7a6364388b3d055f1e91db825
Sneha Sharma <snehav2109@gmail.com>
2025-06-17 21:40:48 +0530

    add user type definitions and authentication input types

---
+++ server/src/graphql/typedefs/user.type.ts
@@ -0,0 +1,40 @@
+export const userTypeDefs = `#graphql
+
+# User Type Definitions
+type User {
+  _id: ID!
+  username: String! 
+  email: String!
+  createdAt: String
+  updatedAt: String}
+
+# Auth Response Type Definitions
+type AuthResponse {
+success: Boolean!
+    message: String
+    user: User
+}
+
+# signup input type
+
+input SignupInput {
+  username: String!
+  email: String!
+  password: String!
+}
+
+input LoginInput {
+  email: String!
+  password: String!
+}
+
+type Query{
+#required for authentication
+    getUser: User
+}
+
+type Mutation {
+createUser(input: SignupInput!): AuthResponse!
+loginUser(input: LoginInput!): AuthResponse!
+}
+`;
