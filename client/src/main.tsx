1113e9881406e5c94b7619e0ab55fd6f48fe6dbf
Sneha Sharma <snehav2109@gmail.com>
2025-06-21 17:00:26 +0530

    feat: add Toaster component

--- client/src/main.tsx
+++ client/src/main.tsx
@@ -3,7 +3,7 @@ import { createRoot } from "react-dom/client";
 
 import "./index.css";
 import App from "./App.tsx";
-
+import { Toaster } from "./components/ui/sonner.tsx";
 import { ApolloProvider } from "@apollo/client";
 import client from "./apollo-client/apollo-client.tsx";
 import { AuthProvider } from "./context/AuthContext.tsx";
@@ -12,6 +12,7 @@ createRoot(document.getElementById("root")!).render(
     <ApolloProvider client={client}>
       <AuthProvider>
         <App />
+        <Toaster />
       </AuthProvider>
     </ApolloProvider>
   </StrictMode>
