39e98f782e5b9c9c1465299aa4014428239d59ff
Sneha Sharma <snehav2109@gmail.com>
2025-06-21 22:30:55 +0530

    fix: update App component to conditionally render

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,9 +1,29 @@
+import Footer from "./components/layout/Footer";
 import Header from "./components/layout/Header";
+import Home from "./components/layout/Home";
+import TodoInputForm from "./components/todo/TodoInputForm";
+import TodoList from "./components/todo/TodoList";
 import { useAuth } from "./context/AuthContext";
 
 function App() {
   const { user } = useAuth();
-  return <Header />;
+  if (user) {
+    return (
+      <>
+        <Header />
+        <TodoInputForm />
+        <TodoList />
+        <Footer />
+      </>
+    );
+  }
+  return (
+    <>
+      <Header />
+      <Home />
+      <Footer />
+    </>
+  );
 }
 
 export default App;
